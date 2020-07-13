816e819e7b97b441d0dcc982d8575555ae19ef2f
Nauara Melo <nauaramelo@gmail.com>
2020-07-13 12:44:28 -0300

    Ajustes na fonte

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'typeface-montserrat';
 
 ReactDOM.render(
   <React.StrictMode>
